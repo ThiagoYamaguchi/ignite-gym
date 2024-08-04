457221d3c7a5ec6fdfd5449525db6ad1bc76b890
“Thiago <thiago.osamu@outlook.com>
2024-08-04 01:42:34 -0300

    Create Input

--- App.tsx
+++ App.tsx
@@ -1,23 +1,20 @@
-import { StatusBar, Text, View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold  } from '@expo-google-fonts/roboto';
+import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed';
+import { config } from './config/gluestack-ui.config';
+import { Loading } from '@components/Loadind';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View 
-      style={{ 
-        flex: 1, 
-        alignItems: 'center', 
-        justifyContent: 'center',
-        backgroundColor: '#202024'
-      }}
-    >
+    <GluestackUIProvider config={config}>
       <StatusBar 
         barStyle='light-content'
         backgroundColor='transparent'
         translucent
       />
-      { fontsLoaded ? <Text>Home</Text> : <View />}
-    </View>
+      { fontsLoaded ? <SignIn /> : <Loading /> }
+    </GluestackUIProvider>
   );
 }
